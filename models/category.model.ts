f8e748414da43c77cc6ca44192ee1bb34e91520d
sinjan08 <sinjan.official@gmail.com>
2026-03-30 10:39:21 +0530

    [bug] deployement error fixed

--- models/category.model.ts
+++ models/category.model.ts
@@ -1,7 +1,43 @@
-import { CategoryInterface } from "@/Interface/category.interface";
-import mongoose, { Model, Schema } from "mongoose";
+// import { CategoryInterface } from "@/Interface/category.interface";
+// import mongoose, { Model, Schema } from "mongoose";
 
-const CategorySchema: Schema<CategoryInterface> = new Schema(
+// const CategorySchema: Schema<CategoryInterface> = new Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//       minlength: 3,
+//       maxlength: 100,
+//     },
+//     description: {
+//       type: String,
+//       trim: true,
+//       maxlength: 500,
+//     },
+//     isDeleted: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     deletedAt: {
+//       type: Date,
+//       default: null,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// const Category: Model<CategoryInterface> =
+//   mongoose.models.Category || mongoose.model<CategoryInterface>("Category", CategorySchema);
+
+// export default Category;
+
+import { CategoryInterface } from "@/Interface/category.interface";
+import { Schema, model, models } from "mongoose";
+
+const CategorySchema = new Schema<CategoryInterface>(
   {
     name: {
       type: String,
@@ -29,7 +65,7 @@ const CategorySchema: Schema<CategoryInterface> = new Schema(
   }
 );
 
-const Category: Model<CategoryInterface> =
-  mongoose.models.Category || mongoose.model<CategoryInterface>("Category", CategorySchema);
+const Category =
+  models.Category || model<CategoryInterface>("Category", CategorySchema);
 
 export default Category;
